9121c0e9632bae0c264a4c57586c68817d530320
Bader Kharbat <bgkharba@ncsu.edu>
2021-11-29 11:42:03 -0500

    progress

--- public/character.js
+++ public/character.js
@@ -31,11 +31,11 @@ export default class character {
         this.right_hip = new part(new THREE.Mesh( new THREE.SphereGeometry(radius, 24, 24), new THREE.MeshStandardMaterial({ color: 0xFF6347 }) ), [this.right_knee]);
         this.left_shoulder = new part(new THREE.Mesh( new THREE.SphereGeometry(radius, 24, 24), new THREE.MeshStandardMaterial({ color: 0xFF6347 }) ), [this.left_elbow]);
         this.right_shoulder = new part(new THREE.Mesh( new THREE.SphereGeometry(radius, 24, 24), new THREE.MeshStandardMaterial({ color: 0xFF6347 }) ), [this.right_elbow]);
-        this.neck = new part(new THREE.Mesh( new THREE.SphereGeometry(radius, 24, 24), new THREE.MeshStandardMaterial({ color: 0xFF6347 }) ), [this.head]);
         this.head = new part(new THREE.Mesh( new THREE.SphereGeometry(7, 24, 24), new THREE.MeshStandardMaterial({ color: 0xFF6347 }) ), []);
+        this.neck = new part(new THREE.Mesh( new THREE.SphereGeometry(radius, 24, 24), new THREE.MeshStandardMaterial({ color: 0xFF6347 }) ), [this.head]);
 
-        this.torso = new part(new THREE.Mesh( new THREE.BoxGeometry( tor_w, tor_h, radius*2 ), new THREE.MeshStandardMaterial( {color: 0x00ff00} ) ), [this.head, this.left_shoulder, this.right_shoulder, this.left_hip, this.right_hip])
-        
+        this.torso = new part(new THREE.Mesh( new THREE.BoxGeometry( tor_w, tor_h, radius*2 ), new THREE.MeshStandardMaterial( {color: 0xffd100} ) ), [this.neck, this.left_shoulder, this.right_shoulder, this.left_hip, this.right_hip])
+
         //positions
         this.left_foot.mesh.position.set(1*tor_w/2-radius,radius,0);
         this.right_foot.mesh.position.set(-1*tor_w/2+radius, radius, 0);
@@ -58,11 +58,24 @@ export default class character {
     }
 
     addToScene(scene) {
-        console.log(this.group.children)
+        // console.log(this.group.children)
+        this.group.castShadow = true;
         scene.add(this.group)
     }
 
+    rotateBody(axis, angle) {
+        axis.normalize();
+
+        this.constructor.rotateChildrenOfObject(this.torso, axis, angle);
+        // this.constructor.rotateAboutPoint(this.torso.mesh, this.torso.position, axis, angle)
+        this.torso.mesh.rotateOnAxis(new THREE.Vector3(axis.x, 0, 0).normalize(), THREE.Math.degToRad(angle));
+        this.torso.mesh.rotateOnAxis(new THREE.Vector3(0, axis.y, 0).normalize(), THREE.Math.degToRad(angle));
+        this.torso.mesh.rotateOnAxis(new THREE.Vector3(0, 0, axis.z).normalize(), THREE.Math.degToRad(angle));
+    }
+
     static rotateAboutPoint(obj, point, axis, theta, pointIsWorld){
+        axis.normalize();
+
         pointIsWorld = (pointIsWorld === undefined)? false : pointIsWorld;
         if(pointIsWorld){
             obj.parent.localToWorld(obj.position); // compensate for world coordinate
@@ -73,10 +86,14 @@ export default class character {
         if(pointIsWorld){
             obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
         }
-        obj.rotateOnAxis(axis, theta); // rotate the OBJECT
+        // obj.rotateOnAxis(axis, theta); // rotate the OBJECT
+        obj.rotateOnAxis(new THREE.Vector3(axis.x, 0, 0).normalize(), THREE.Math.degToRad(theta));
+        obj.rotateOnAxis(new THREE.Vector3(0, axis.y, 0).normalize(), THREE.Math.degToRad(theta));
+        obj.rotateOnAxis(new THREE.Vector3(0, 0, axis.z).normalize(), THREE.Math.degToRad(theta));
     }
     
     static rotateChildrenOfObject(parentObject, axis, angle) {
+        axis.normalize();
         for (let child of parentObject.children) {
             //first rotate children recursively
             this.recursiveHelper(child, parentObject, axis, angle)
--- public/render.js
+++ public/render.js
@@ -3,6 +3,7 @@ import { OrbitControls } from '/jsm/controls/OrbitControls.js';
 import "/jsm/libs/stats.module.js";
 // import { FontLoader } from '/FontLoader.js';
 import Character from '/character.js';
+import { GUI } from '/dat.gui.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
@@ -17,8 +18,12 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(200);
 camera.position.setY(200);
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(ambientLight)
+const light = new THREE.DirectionalLight( 0xffffff, 0.5, 100 );
+const light2 = new THREE.AmbientLight( 0xffffff, 0.5, 100 );
+light.position.y = 200
+light.position.z = 300
+light.castShadow = true;
+scene.add(light, light2)
 
 var character = new Character();
 character.addToScene(scene);
@@ -34,19 +39,45 @@ function addPlane() {
   front.position.set(0, -5, length / 2)
   var back = new THREE.Mesh(new THREE.BoxGeometry(width, 10, length), new THREE.MeshStandardMaterial({ color: 0xE2E5DE }))
   back.position.set(0, -5, -1 * length / 2)
+  front.receiveShadow = true;
+  back.receiveShadow = true;
   scene.add(front, back)
 }
 addPlane()
 
-Character.rotateChildrenOfObject(character.right_shoulder, new THREE.Vector3(0, 1, 0), 75);
-Character.rotateChildrenOfObject(character.left_shoulder, new THREE.Vector3(0, 1, 0), 75);
+// const gui = new GUI()
+// const cubeFolder = gui.addFolder('Cube')
+// cubeFolder.add(character.torso.mesh.rotation, 'x', 0, Math.PI * 2)
+// cubeFolder.add(character.torso.mesh.rotation, 'y', 0, Math.PI * 2)
+// cubeFolder.add(character.torso.mesh.rotation, 'z', 0, Math.PI * 2)
+// cubeFolder.open()
 
+// Character.rotateChildrenOfObject(character.right_shoulder, new THREE.Vector3(0, 1, 0), 75);
+// Character.rotateChildrenOfObject(character.left_shoulder, new THREE.Vector3(0, 0, 1), 75);
+
+function createCircle(size, color, xR, yR, zR, x, y, z) {
+  const geometry = new THREE.CircleGeometry( size, 100 );
+  const material = new THREE.MeshBasicMaterial( { color: color, side: THREE.DoubleSide } );
+  const circle = new THREE.Mesh( geometry, material );
+  circle.position.set(x, y, z)
+  circle.rotation.x = xR
+  circle.rotation.y = yR
+  circle.rotation.z = zR
+  scene.add( circle );
+}
+createCircle(10, 0x87ff00, 0, 0, Math.PI / 2, character.right_shoulder.mesh.position.x, character.right_shoulder.mesh.position.y, character.right_shoulder.mesh.position.z)
+createCircle(10, 0xff0000, 0, Math.PI / 2, 0, character.right_shoulder.mesh.position.x, character.right_shoulder.mesh.position.y, character.right_shoulder.mesh.position.z)
+createCircle(10, 0x3e00ff, Math.PI / 2, 0, 0, character.right_shoulder.mesh.position.x, character.right_shoulder.mesh.position.y, character.right_shoulder.mesh.position.z)
 
 function animate() {
   requestAnimationFrame(animate);
 
-  Character.rotateChildrenOfObject(character.right_shoulder, new THREE.Vector3(1, 0, 0), -5);
-  Character.rotateChildrenOfObject(character.left_shoulder, new THREE.Vector3(1, 0, 0), -5);
+  // character.torso.mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), THREE.Math.degToRad(5));
+  // character.torso.mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), THREE.Math.degToRad(5));
+
+  // character.rotateBody(new THREE.Vector3(1, 1, 0), -2)
+  // Character.rotateChildrenOfObject(character.right_shoulder, new THREE.Vector3(1, 1, 0), -5);
+  // Character.rotateChildrenOfObject(character.torso, new THREE.Vector3(1, 1, 0), -5);
 
   controls.update();
   renderer.render(scene, camera);
