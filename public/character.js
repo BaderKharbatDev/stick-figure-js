94250a47b1953dc9be8181dcdbf1455be6c4a346
Bader Kharbat <bgkharba@ncsu.edu>
2021-11-29 19:31:46 -0500

    mor prog

--- public/character.js
+++ public/character.js
@@ -1,4 +1,7 @@
 import * as THREE from '/build/three.module.js';
+import { GUI } from '/dat.gui.module.js';
+
+let gui;
 
 class part {
     constructor( mesh, children, distanceFromParent ) {
@@ -10,6 +13,7 @@ class part {
 }
 
 export default class character {
+
     constructor() {
         let radius = 3;
         let arm_d = 20;
@@ -34,7 +38,7 @@ export default class character {
         this.head = new part(new THREE.Mesh( new THREE.SphereGeometry(7, 24, 24), new THREE.MeshStandardMaterial({ color: 0xFF6347 }) ), []);
         this.neck = new part(new THREE.Mesh( new THREE.SphereGeometry(radius, 24, 24), new THREE.MeshStandardMaterial({ color: 0xFF6347 }) ), [this.head]);
 
-        this.torso = new part(new THREE.Mesh( new THREE.BoxGeometry( tor_w, tor_h, radius*2 ), new THREE.MeshStandardMaterial( {color: 0xffd100} ) ), [this.neck, this.left_shoulder, this.right_shoulder, this.left_hip, this.right_hip])
+        this.torso = new part(new THREE.Mesh( new THREE.SphereGeometry( radius, 24, 24 ), new THREE.MeshStandardMaterial( {color: 0xffd100} ) ), [this.neck, this.left_shoulder, this.right_shoulder, this.left_hip, this.right_hip])
 
         //positions
         this.left_foot.mesh.position.set(1*tor_w/2-radius,radius,0);
@@ -53,24 +57,41 @@ export default class character {
         this.neck.mesh.position.set(0,this.left_hip.mesh.position.y+tor_h,0);
         this.head.mesh.position.set(0,this.neck.mesh.position.y+arm_d,0);
 
+        // var plane = new THREE.PlaneGeometry( tor_w, tor_h, 1 );
+        // this.torso_wall = new THREE.Mesh( plane, new THREE.MeshBasicMaterial( { color: 0x00ff00, side: THREE.DoubleSide} ) );
+        // this.torso_wall.position.set(this.torso.mesh.position.x, this.torso.mesh.position.y, this.torso.mesh.position.z);
+
+        // var plane = new THREE.Plane();
+        // plane.setFromCoplanarPoints(new THREE.Vector3(),new THREE.Vector3(),new THREE.Vector3())
+        // var geometry = new THREE.PlaneGeometry(100, 100);
+        // var mesh = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({ color: 0xFF6347 }));
+        // mesh.translate(plane.coplanarPoint());
+        // mesh.quaternion.setFromUnitVectors(new THREE.Vector3(0,0,1), plane.normal);
+
         this.group = new THREE.Group();
         this.group.add(this.torso.mesh, this.head.mesh, this.neck.mesh, this.left_shoulder.mesh, this.right_shoulder.mesh, this.left_hip.mesh, this.right_hip.mesh, this.right_elbow.mesh, this.left_elbow.mesh, this.right_knee.mesh, this.left_knee.mesh, this.right_hand.mesh, this.left_hand.mesh, this.right_foot.mesh, this.left_foot.mesh)
+        this.torso.mesh.visible = false
     }
 
     addToScene(scene) {
-        // console.log(this.group.children)
-        this.group.castShadow = true;
         scene.add(this.group)
     }
 
-    rotateBody(axis, angle) {
-        axis.normalize();
+    
+    //helpers
+    updateTorso() {
+        // var box = new THREE.Box3().setFromObject( points );
 
-        this.constructor.rotateChildrenOfObject(this.torso, axis, angle);
-        // this.constructor.rotateAboutPoint(this.torso.mesh, this.torso.position, axis, angle)
-        this.torso.mesh.rotateOnAxis(new THREE.Vector3(axis.x, 0, 0).normalize(), THREE.Math.degToRad(angle));
-        this.torso.mesh.rotateOnAxis(new THREE.Vector3(0, axis.y, 0).normalize(), THREE.Math.degToRad(angle));
-        this.torso.mesh.rotateOnAxis(new THREE.Vector3(0, 0, axis.z).normalize(), THREE.Math.degToRad(angle));
+    }
+
+    rotateTorso(axis, angle) {
+        axis.normalize();
+        this.constructor.rotateChildrenOfObject(this.torso, axis, angle)
+        console.log(axis)
+        // this.torso_wall.rotation.x += THREE.Math.degToRad(angle) * axis.x
+        // this.torso_wall.rotation.y += THREE.Math.degToRad(angle) * axis.y
+        // this.torso_wall.rotation.z += THREE.Math.degToRad(angle) * axis.z
+        // this.torso_wall.rotation.set
     }
 
     static rotateAboutPoint(obj, point, axis, theta, pointIsWorld){
@@ -86,7 +107,6 @@ export default class character {
         if(pointIsWorld){
             obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
         }
-        // obj.rotateOnAxis(axis, theta); // rotate the OBJECT
         obj.rotateOnAxis(new THREE.Vector3(axis.x, 0, 0).normalize(), THREE.Math.degToRad(theta));
         obj.rotateOnAxis(new THREE.Vector3(0, axis.y, 0).normalize(), THREE.Math.degToRad(theta));
         obj.rotateOnAxis(new THREE.Vector3(0, 0, axis.z).normalize(), THREE.Math.degToRad(theta));
@@ -95,14 +115,12 @@ export default class character {
     static rotateChildrenOfObject(parentObject, axis, angle) {
         axis.normalize();
         for (let child of parentObject.children) {
-            //first rotate children recursively
             this.recursiveHelper(child, parentObject, axis, angle)
         }
     }
     
     static recursiveHelper(object, pivotObject, axis, angle) {
         for (let child of object.children) {
-            //first rotate children recursively
             this.recursiveHelper(child, pivotObject, axis, angle)
         }
         this.rotateObjectAroundObject(object, pivotObject, axis, angle)
@@ -111,6 +129,55 @@ export default class character {
     static rotateObjectAroundObject(rotatingObject, pivotObject, axis, angle) {
         this.rotateAboutPoint(rotatingObject.mesh, pivotObject.mesh.position, axis, THREE.Math.degToRad(angle)) 
     }
+
+    static openMenu(part) {
+        gui = new GUI();
+        const xFolder = gui.addFolder('X Axis')
+        var x = { 
+            AnglePos:function(){
+
+            },
+            AngleNeg:function(){
+
+            }
+        };
+        xFolder.add(x, 'AnglePos')
+        xFolder.add(x, 'AngleNeg')
+        const yFolder = gui.addFolder('Y Axis')
+        var y = { 
+            AnglePos:function(){
+
+            },
+            AngleNeg:function(){
+
+            }
+        };
+        yFolder.add(y, 'AnglePos')
+        yFolder.add(y, 'AngleNeg')
+        const zFolder = gui.addFolder('Z Axis')
+        var z = { 
+            AnglePos:function(){
+
+            },
+            AngleNeg:function(){
+
+            }
+        };
+        zFolder.add(z, 'AnglePos')
+        zFolder.add(z, 'AngleNeg')
+        xFolder.open()
+        yFolder.open()
+        zFolder.open()
+    }
+
+    static closeMenu() {
+        gui.destroy();
+        // GUI.toggleHide();
+    }
 }
+
+
+
+
 
 
--- public/render.js
+++ public/render.js
@@ -1,22 +1,25 @@
 import * as THREE from '/build/three.module.js';
 import { OrbitControls } from '/jsm/controls/OrbitControls.js';
 import "/jsm/libs/stats.module.js";
-// import { FontLoader } from '/FontLoader.js';
 import Character from '/character.js';
 import { GUI } from '/dat.gui.module.js';
 
+//init
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
-
 const renderer = new THREE.WebGLRenderer({
+  antialias: true,
   canvas: document.querySelector('#bg'),
   alpha: true
 });
-
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(200);
 camera.position.setY(200);
+
+//scene helpers
+const axesHelper = new THREE.AxesHelper( 200 );
+scene.add( axesHelper );
 
 const light = new THREE.DirectionalLight( 0xffffff, 0.5, 100 );
 const light2 = new THREE.AmbientLight( 0xffffff, 0.5, 100 );
@@ -25,13 +28,12 @@ light.position.z = 300
 light.castShadow = true;
 scene.add(light, light2)
 
+//adds character
 var character = new Character();
 character.addToScene(scene);
+character.head.mesh.callback = function() { console.log( 'head clicked' ); }
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.target = character.torso.mesh.position
-controls.update
-
+//floor
 function addPlane() {
   let length = 5000;
   let width = 10000;
@@ -45,32 +47,85 @@ function addPlane() {
 }
 addPlane()
 
-// const gui = new GUI()
-// const cubeFolder = gui.addFolder('Cube')
-// cubeFolder.add(character.torso.mesh.rotation, 'x', 0, Math.PI * 2)
-// cubeFolder.add(character.torso.mesh.rotation, 'y', 0, Math.PI * 2)
-// cubeFolder.add(character.torso.mesh.rotation, 'z', 0, Math.PI * 2)
-// cubeFolder.open()
+//on object click
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
 
-// Character.rotateChildrenOfObject(character.right_shoulder, new THREE.Vector3(0, 1, 0), 75);
-// Character.rotateChildrenOfObject(character.left_shoulder, new THREE.Vector3(0, 0, 1), 75);
+var selected_obj = null;
+var old_color = null;
 
-function createCircle(size, color, xR, yR, zR, x, y, z) {
-  const geometry = new THREE.CircleGeometry( size, 100 );
-  const material = new THREE.MeshBasicMaterial( { color: color, side: THREE.DoubleSide } );
-  const circle = new THREE.Mesh( geometry, material );
-  circle.position.set(x, y, z)
-  circle.rotation.x = xR
-  circle.rotation.y = yR
-  circle.rotation.z = zR
-  scene.add( circle );
+function onDocumentMouseDown( event ) {
+    event.preventDefault();
+    mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+    raycaster.setFromCamera( mouse, camera );
+    var intersects = raycaster.intersectObjects( character.group.children ); 
+    if ( intersects.length > 0 ) {
+      try {
+        if(selected_obj != null) {
+          selected_obj.material.color.setHex(old_color)
+          Character.closeMenu();
+        }
+        if(intersects[0].object == selected_obj) {
+          selected_obj.material.color.setHex(old_color)
+          selected_obj = null;
+          old_color = null;
+          Character.closeMenu();
+        } else {
+          selected_obj = intersects[0].object;
+          old_color = JSON.parse(JSON.stringify(selected_obj.material.color));
+          selected_obj.material.color.setHex(0x000000);
+          Character.openMenu(selected_obj);
+        }
+      } catch (error) {
+        // console.log(error)
+      }
+    }
 }
-createCircle(10, 0x87ff00, 0, 0, Math.PI / 2, character.right_shoulder.mesh.position.x, character.right_shoulder.mesh.position.y, character.right_shoulder.mesh.position.z)
-createCircle(10, 0xff0000, 0, Math.PI / 2, 0, character.right_shoulder.mesh.position.x, character.right_shoulder.mesh.position.y, character.right_shoulder.mesh.position.z)
-createCircle(10, 0x3e00ff, Math.PI / 2, 0, 0, character.right_shoulder.mesh.position.x, character.right_shoulder.mesh.position.y, character.right_shoulder.mesh.position.z)
+window.addEventListener('mousedown', onDocumentMouseDown)
+
+//controls
+var controls = new OrbitControls(camera, renderer.domElement)
+controls.target = character.torso.mesh.position
+controls.enablePan = false;
+controls.update
+
+// function createCircle(size, color, xR, yR, zR, x, y, z) {
+//   const geometry = new THREE.CircleGeometry( size, 100 );
+//   const material = new THREE.MeshBasicMaterial( { color: color, side: THREE.DoubleSide } );
+//   const circle = new THREE.Mesh( geometry, material );
+//   circle.position.set(x, y, z)
+//   circle.rotation.x = xR
+//   circle.rotation.y = yR
+//   circle.rotation.z = zR
+//   scene.add( circle );
+// }
+// createCircle(10, 0x87ff00, 0, 0, Math.PI / 2, character.right_shoulder.mesh.position.x, character.right_shoulder.mesh.position.y, character.right_shoulder.mesh.position.z)
+// createCircle(10, 0xff0000, 0, Math.PI / 2, 0, character.right_shoulder.mesh.position.x, character.right_shoulder.mesh.position.y, character.right_shoulder.mesh.position.z)
+// createCircle(10, 0x3e00ff, Math.PI / 2, 0, 0, character.right_shoulder.mesh.position.x, character.right_shoulder.mesh.position.y, character.right_shoulder.mesh.position.z)
+
+// document.addEventListener("keydown", onDocumentKeyDown, false);
+// function onDocumentKeyDown(event) {
+//     var keyCode = event.which;
+//     if (keyCode == 87) { //w
+//       character.rotateTorso(new THREE.Vector3(1, 0, 0), -10)
+//     } else if (keyCode == 83) { //s
+//       character.rotateTorso(new THREE.Vector3(1, 0, 0), 10)
+//     } else if (keyCode == 65) { //a
+//       character.rotateTorso(new THREE.Vector3(0, 1, 0), -10)
+//     } else if (keyCode == 68) { //d
+//       character.rotateTorso(new THREE.Vector3(0, 1, 0), 10)
+//     }
+// };
+
+//slider
+// var slider = document.getElementById("slider");
+// slider.addEventListener("input", null);
 
 function animate() {
   requestAnimationFrame(animate);
+
+  // character.updateTorso(scene)
 
   // character.torso.mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), THREE.Math.degToRad(5));
   // character.torso.mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), THREE.Math.degToRad(5));
